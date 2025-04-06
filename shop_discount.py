80f66817a09d8d8790ddb3543546da5f02fd908f
tommyquacks <159901224+tommyquacks@users.noreply.github.com>
2025-04-06 13:30:03 +0100

    Create shop_discount.py

---
+++ shop_discount.py
@@ -0,0 +1,6 @@
+total_spent = float(input("What is the total cost of your items?")
+                    if total_spent < 50
+                    print(f"your total is: {total_spent}
+                    elif 200 > total_spend > 50
+                    total_spend = total_spend * 0.9
+                    print(f"your total is: {total_spent}
